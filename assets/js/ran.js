07b0150c947112a4d45e66941f228e2a71e112f5
yym0807 <75062543+yym0807@users.noreply.github.com>
2022-04-22 01:11:09 +0800

    XD

--- assets/js/ran.js
+++ assets/js/ran.js
@@ -1,6 +1,6 @@
-var keys=["./M/M01","./M/M02","./M/M03","./M/M04","./M/M05","./M/M06","./P/P01","./P/P02","./P/P03","./P/P04","./B/B01","./B/B02","./B/B03","./B/B04","./B/B05","./B/B06","./B/B07","./B/B08","./CH/CH01","./CH/CH02","./CH/CH03","./CH/CH04","./I/I01","./I/I02","./I/I03","./I/I04","./I/I05","./I/I06","./G/G01","./G/G02"]
+var keys=["./M/M01","./M/M02","./M/M03","./M/M04","./M/M05","./M/M06","./P/P01","./P/P02","./P/P03","./P/P04","./B/B01","./B/B02","./B/B03","./B/B04","./B/B05","./B/B06","./B/B07","./B/B08","./CH/CH01","./CH/CH02","./CH/CH03","./CH/CH04","./I/I01","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I02","./I/I03","./I/I04","./I/I05","./I/I06","./G/G01","./G/G02"]
 // str="<a href='"+keys[Math.floor(Math.random()*30))]+" class='button primary'>";
 // document.write("This is written from JavaScript");
 // document.write("<a href='./M/M01'"+">321"+"</a>");
-document.write("<a href='"+keys[Math.floor(Math.random()*30)]+"' class='button primary'>隨機作品</a>");
+document.write("<a href='"+keys[Math.floor(Math.random()*49)]+"' class='button primary'>隨機作品</a>");
 // document.write(keys[Math.floor(Math.random()*30)]);
