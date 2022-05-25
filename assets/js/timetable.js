b9a05ca2ea2633a141309b8f088107cb39f6ba64
ExuberantIris <imanthony1010@gmail.com>
2022-05-25 10:29:31 +0800

    改成連結

--- assets/js/timetable.js
+++ assets/js/timetable.js
@@ -122,7 +122,10 @@ function change(IST) {
     tem[5].innerHTML = list3[ntcII * 2 + 1];
   }
   if(Now == 251300 || (Now >= 251300 && IST)){
-    let ifr = Body.querySelector(".wait");
-    ifr.setAttribute("src", "https://www.youtube.com/embed/bscCx0Zg2X0");
+    //let ifr = Body.querySelector(".wait");
+    //ifr.setAttribute("src", "https://www.youtube.com/embed/bscCx0Zg2X0");
+    let a = Body.querySelector(".w");
+    //a.style.color = 'blue';
+    a.setAttribute("href", "https://youtu.be/bscCx0Zg2X0");
   }
 }
